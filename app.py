8862a66d5aee87d5d6f26da0d1c2b016917a3509
strash <n.strashnikov@gmail.com>
2023-04-08 13:03:26 +0300

    fix2

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-# Mock API call that returns some data
-def example_api():
-    return {"content": "some response"}
-
-# Route that serves the HTML page with the text field
-@app.route('/start', methods=['GET'])
-def start():
+@app.route('/')
+def index():
     return render_template('index.html')
-
-# Endpoint that is called when the text field is submitted
-@app.route('/enrich', methods=['POST'])
-def enrich():
-    # Get the value of the text field
-    text = request.form['text']
-
-    # Call the mock API
-    response = example_api()
-
-    # Enrich the response and return it as JSON
-    final_content = "enriched {}".format(response['content'])
-    return jsonify({"finalcon": final_content})
 
 if __name__ == '__main__':
     app.run()
